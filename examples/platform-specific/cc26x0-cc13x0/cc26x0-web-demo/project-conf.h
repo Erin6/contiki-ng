57856d100fd53bee5486bde19a26df12a73edb49
yunzhewu <yunzhe.wu@hotmail.com>
2020-04-15 10:12:03 +0100

    Add files via upload

--- examples/platform-specific/cc26x0-cc13x0/cc26x0-web-demo/project-conf.h
+++ examples/platform-specific/cc26x0-cc13x0/cc26x0-web-demo/project-conf.h
@@ -42,9 +42,9 @@
 
 /* Enable/Disable Components of this Demo */
 #define CC26XX_WEB_DEMO_CONF_MQTT_CLIENT      1
-#define CC26XX_WEB_DEMO_CONF_6LBR_CLIENT      ROUTING_CONF_RPL_CLASSIC
-#define CC26XX_WEB_DEMO_CONF_COAP_SERVER      1
-#define CC26XX_WEB_DEMO_CONF_NET_UART         1
+#define CC26XX_WEB_DEMO_CONF_6LBR_CLIENT      0
+#define CC26XX_WEB_DEMO_CONF_COAP_SERVER      0
+#define CC26XX_WEB_DEMO_CONF_NET_UART         0
 
 /*
  * ADC sensor functionality. To test this, an external voltage source should be
